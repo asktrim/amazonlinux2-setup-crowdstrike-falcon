5001ae48d5d713a3e1e65854c4c2c0a5a4d7c6ca
Ryan Mark <ryan@mrk.cc>
2021-08-27 16:07:01 -0700

    load params from ssm

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export $(aws ssm get-parameters --region "us-east-1" --with-decryption --names '/crowdstrike/CS_API_CLIENT_ID' '/crowdstrike/CS_API_CLIENT_SECRET' '/crowdstrike/CLOUDSIM_CID' | python -c"import json, sys; print reduce(lambda m, x: \"\n\".join([m, \"%s=%s\" % (x['Name'].split('/')[-1],x['Value'])]), json.loads(sys.stdin.read())['Parameters'], '')")
+
 curl -fsSL https://raw.githubusercontent.com/asktrim/amazonlinux2-setup-crowdstrike-falcon/main/download-falcon.py -o /tmp/download-falcon.py
 
 python /tmp/download-falcon.py /tmp/falcon-sensor.rpm
@@ -12,3 +14,5 @@ yum -y install /tmp/falcon-sensor.rpm
 
 systemctl enable falcon-sensor.service
 systemctl start falcon-sensor.service
+
+rm /tmp/downlod-falcon.py /tmp/falcon-sensor.rpm
