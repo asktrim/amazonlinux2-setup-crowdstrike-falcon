011bad787390cd8e998338f944a404eca99d0c52
Ryan Mark <ryan@mrk.cc>
2021-09-01 11:16:04 -0700

    include tags in crowdstrike install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export $(aws ssm get-parameters --region "us-east-1" --with-decryption --names '/crowdstrike/CS_API_CLIENT_ID' '/crowdstrike/CS_API_CLIENT_SECRET' '/crowdstrike/CLOUDSIM_CID' | python -c"import json, sys; print reduce(lambda m, x: \"\n\".join([m, \"%s=%s\" % (x['Name'].split('/')[-1],x['Value'])]), json.loads(sys.stdin.read())['Parameters'], '')")
+export $(aws ssm get-parameters --region "us-east-1" --with-decryption --names '/crowdstrike/CS_API_CLIENT_ID' '/crowdstrike/CS_API_CLIENT_SECRET' '/crowdstrike/CLOUDSIM_CID' '/crowdstrike/CLOUDSIM_TAGS' | python -c"import json, sys; print reduce(lambda m, x: \"\n\".join([m, \"%s=%s\" % (x['Name'].split('/')[-1],x['Value'])]), json.loads(sys.stdin.read())['Parameters'], '')")
 
 curl -fsSL https://raw.githubusercontent.com/asktrim/amazonlinux2-setup-crowdstrike-falcon/main/download-falcon.py -o /tmp/download-falcon.py
 
@@ -10,7 +10,7 @@ python /tmp/download-falcon.py /tmp/falcon-sensor.rpm
 
 yum -y install /tmp/falcon-sensor.rpm
 
-/opt/CrowdStrike/falconctl -s -f --cid=$CLOUDSIM_CID
+/opt/CrowdStrike/falconctl -s -f --cid=$CLOUDSIM_CID --tags=$CLOUDSIM_TAGS
 
 systemctl enable falcon-sensor.service
 systemctl start falcon-sensor.service
