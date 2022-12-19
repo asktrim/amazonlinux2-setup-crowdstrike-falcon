c64ea767851d3a677a80988aece052f499271023
jdf400 <jdf400@users.noreply.github.com>
2022-12-19 13:44:58 -0500

    use yum-cron to setup security updates (#1)
    * use yum-cron to setup security updates

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ set -e
 
 export $(aws ssm get-parameters --region "us-east-1" --with-decryption --names '/crowdstrike/CS_API_CLIENT_ID' '/crowdstrike/CS_API_CLIENT_SECRET' '/crowdstrike/CLOUDSIM_CID' '/crowdstrike/CLOUDSIM_TAGS' | python -c"import json, sys; print reduce(lambda m, x: \"\n\".join([m, \"%s=%s\" % (x['Name'].split('/')[-1],x['Value'])]), json.loads(sys.stdin.read())['Parameters'], '')")
 
+sudo yum install yum-cron
+
 curl -fsSL https://raw.githubusercontent.com/asktrim/amazonlinux2-setup-crowdstrike-falcon/main/download-falcon.py -o /tmp/download-falcon.py
+# Overwrite yum-cron.conf sections
+sudo sed -i "s/apply_updates = no/apply_updates = yes/" /etc/yum/yum-cron.conf
+sudo sed -i "s/update_cmd = default/update_cmd = security/" /etc/yum/yum-cron.conf
 
 python /tmp/download-falcon.py /tmp/falcon-sensor.rpm
 
@@ -14,3 +19,5 @@ yum -y install /tmp/falcon-sensor.rpm
 
 systemctl enable falcon-sensor.service
 systemctl start falcon-sensor.service
+sudo service yum-cron start
+sudo systemctl enable yum-cron
