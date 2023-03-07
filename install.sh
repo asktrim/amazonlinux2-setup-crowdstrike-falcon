b919fd631399b297f28e075ebbeeae03e39a598b
Ryan Mark <ryan@mrk.cc>
2023-03-07 13:05:58 -0800

    make sure every command that needs cloudstrike creds runs after we try to get the creds

--- install.sh
+++ install.sh
@@ -17,18 +17,18 @@ sudo systemctl start yum-cron
 sudo yum kernel-livepatch enable -y
 sudo amazon-linux-extras enable livepatch
 
-# Download falcon download script
-curl -fsSL https://raw.githubusercontent.com/asktrim/amazonlinux2-setup-crowdstrike-falcon/main/download-falcon.py -o /tmp/download-falcon.py
-
-# run download script
-python /tmp/download-falcon.py /tmp/falcon-sensor.rpm
-
-# install and configure falcon
-sudo yum -y install /tmp/falcon-sensor.rpm
-
 if command -v aws &> /dev/null
 then
   export $(aws ssm get-parameters --region "us-east-1" --with-decryption --names '/crowdstrike/CS_API_CLIENT_ID' '/crowdstrike/CS_API_CLIENT_SECRET' '/crowdstrike/CLOUDSIM_CID' '/crowdstrike/CLOUDSIM_TAGS' | python -c"import json, sys; print reduce(lambda m, x: \"\n\".join([m, \"%s=%s\" % (x['Name'].split('/')[-1],x['Value'])]), json.loads(sys.stdin.read())['Parameters'], '')")
+
+  # Download falcon download script
+  curl -fsSL https://raw.githubusercontent.com/asktrim/amazonlinux2-setup-crowdstrike-falcon/main/download-falcon.py -o /tmp/download-falcon.py
+
+  # run download script
+  python /tmp/download-falcon.py /tmp/falcon-sensor.rpm
+
+  # install and configure falcon
+  sudo yum -y install /tmp/falcon-sensor.rpm
 
   sudo /opt/CrowdStrike/falconctl -s -f --cid=$CLOUDSIM_CID --tags=$CLOUDSIM_TAGS --trace=debug --feature=enableLog --message-log=true
 
